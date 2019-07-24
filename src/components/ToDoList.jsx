4d564db5225b85d048c4f90a3cd4c71990f2e0bc
Rene Wensel <hey@renewensel.com>
2019-07-24 18:37:54 +0200

    created ToDoList component

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,9 @@
+import React, { Component, Fragment } from "react";
+
+class ToDoList extends Component {
+    render() {
+        return (
+            <div className="alert alert-info">This is where the ToDo items will be</div>
+        )
+    }
+}
