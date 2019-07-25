66ba76e5e214723ce0fd29d65af331e8b23ade25
Rene Wensel <hey@renewensel.com>
2019-07-25 19:32:01 +0200

    created Router Component

---
+++ src/components/Router.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import App from "./App";
+import Help from "./Help";
+import NotFound from "./NotFound";
+
+const Router = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/help" component={Help} />
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Router;
