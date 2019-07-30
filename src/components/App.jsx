df681ee6a4f786444d88937f451a61ab7ced4f62
Rene Wensel <hey@renewensel.com>
2019-07-30 20:23:59 +0200

    created ToDoItem component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,37 @@
 import React from "react";
 import Header from "./Header";
+import ToDoForm from "./ToDoForm";
 import ToDoList from "./ToDoList";
+import uuid from "uuid/v4";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            toDoItems: {}
+        };
+    }
+
+    addToDo = text => {
+        const todo = {
+            uuid: uuid(),
+            text: text,
+            done: false
+        };
+
+        this.setState(state => {
+            state.toDoItems[todo.uuid] = todo;
+            return state;
+        });
+    };
+
     render() {
         return (
             <div className="container">
-                <Header />
-                <ToDoList />
+                <Header tagline="Here are all the next tasks." />
+                <ToDoForm addToDo={this.addToDo} />
+                <ToDoList items={this.state.toDoItems} />
             </div>
         );
     }
---
+++ src/components/ToDoForm.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+class ToDoForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    textInput = React.createRef();
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const text = this.textInput.current.value.trim();
+        this.props.addToDo(text);
+        e.currentTarget.reset();
+        console.log(`Create new item: ${this.textInput.current.value}`);
+    }
+
+
+    render() {
+        return (
+            <form className="input-group my-3" onSubmit={this.handleSubmit}>
+                <input
+                    className="form-control"
+                    name="name"
+                    type="text"
+                    placeholder="Add a new to-do item ..."
+                    ref={this.textInput}
+                />
+                <div className="input-group-append">
+                    <button className="btn btn-outline-secondary" type="submit">
+                        <i className="fas fa-plus" aria-hidden="true" />&nbsp;Add item
+          </button>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default ToDoForm;
