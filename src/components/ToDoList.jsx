f1b58225f25573a3d7d270e4870536f24dac549a
Rene Wensel <hey@renewensel.com>
2019-07-31 12:35:12 +0200

    updated styles

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import ToDoItem from "./ToDoItem";
 
 class ToDoList extends Component {
     render() {
         return (
-            <div className="alert alert-info">This is where the ToDo items will be</div>
+            <div className="todo-list">
+                <ul className="todo-items">
+                    {Object.keys(this.props.items).map(uuid => (
+                        <ToDoItem key={`todo-item-${uuid}`} data={this.props.items[uuid]} />
+                    ))}
+                </ul>
+            </div>
         )
     }
 }
